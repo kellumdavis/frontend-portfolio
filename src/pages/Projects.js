b7345f228c435e564a13c621ee63458d172a54e0
kellumdavis <kellumdavis@gmail.com>
2022-05-11 15:04:16 -0400

    cleaned up code

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,21 +1,20 @@
 import { useState, useEffect } from "react";
-
+import React from "react";
 function Projects(props) {
   // create state to hold projects
   const [projects, setProjects] = useState(null);
 
   //create function to make api call
-  const getProjectsData = async () => {
-    //make api call and get response
-    const response = await fetch(props.URL + "projects");
-    // turn response into javascript object
-    const data = await response.json();
-    // set the projects state to the data
-    setProjects(data);
-  };
+ 
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
   useEffect(()=> {
+    const getProjectsData = async () => {
+        const response = await fetch(props.URL + "projects");
+        const data = await response.json();
+        console.log(data)
+        setProjects(data)
+    };
     getProjectsData()}, [props.URL]);
 
   // define a function that will return the JSX needed once we get the data
